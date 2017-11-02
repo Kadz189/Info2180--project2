529abdf224781508f342c12cb731d7334ea4b250
Kadz189 <32443312+Kadz189@users.noreply.github.com>
2017-11-02 12:57:31 -0500

    Add files via upload

--- fifteen.js
+++ fifteen.js
@@ -1,33 +1,115 @@
+/*
+
+Kadeem Samuda - 620075599
+Project 2 - 15 Puzzle
+
+Extra Feature(s) - Animation and number of moves
+
+
+*/
+
+
 "use strict";
+window.onload = function(){
+var tile = document.querySelectorAll("#puzzlearea div");
+var xpos=0;
+var ypos= 0;
+var BLANKTOP= "300px";
+var BLANKLEFT= "300px";
+var BLOCKTOP, BLOCKLEFT;
+var moveCount = 0;
+var startTime, endTime;
 
-var COLS = 4;
-var emptyRow = 3;
-var emptyCol = 3;
-var SQSIZE = 100;
 
 
-$(function () {
-    var parent = $("#puzzlearea");
-    var divs = parent.children();
-    while (divs.length) {
-        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+
+
+
+    for(var i=0; i < tile.length; i++){
+        tile[i].setAttribute("class", "puzzlepiece") ;
+        tile[i].style.left= xpos + 'px';
+        tile[i].style.top= ypos + 'px';
+        tile[i].style.backgroundPosition= "-" + xpos + "px " + "-" + ypos + "px";
+        if (xpos < 300){
+          xpos+=100;
+        }
+        else{
+            xpos=0;
+            ypos+=100;
+        }
     }
-});
+
+    var shufflebutton= document.getElementById('shufflebutton');
+    shufflebutton.addEventListener("click", shuffle);
+     for(var i=0; i < tile.length; i++){
+          (function(index) {
+            tile[index].addEventListener("mouseover", function(){
+            validMove(this);
+            });
+            tile[index].addEventListener("click", function(){
+            if (validMove(this)){
+                     move(tile[index]);
+            }
+            });
+            tile[index].addEventListener("mouseout", function(){
+                       this.setAttribute("class", "puzzlepiece");
+            });
+          })(i);
+
+     }
+
+     function shuffle(){
+         var piece;
+         for (var i=0; i<100; i++){
+                 piece=  Math.floor(Math.random() * 15);
+                 move(tile[piece]);
+         }
+     }
 
 
-function pageLoad() {
-  createPuzzle();
-  document.getElementById("shufflebutton").onclick = shufflePuzzle();
-}
+    function move(puzzlepiece){
+            BLOCKTOP=puzzlepiece.offsetTop;
+            BLOCKLEFT=puzzlepiece.offsetLeft;
+            puzzlepiece.setAttribute("id", "selected");
+            $('#selected').animate({
+                backgroundImage: "url(background.jpg)",
+                border: "2px solid black",
+                height: "96px",
+                lineHeight: "96px",
+                position: "absolute",
+                textAlign: "center",
+                verticalAlign: "middle",
+                width: "96px",
+                left: BLANKLEFT,
+                top: BLANKTOP
+             });
+              puzzlepiece.style.top = BLANKTOP;
+              puzzlepiece.style.left = BLANKLEFT;
+              BLANKTOP= BLOCKTOP + "px";
+              BLANKLEFT=BLOCKLEFT + "px";
+              puzzlepiece.removeAttribute("id");
+    }
 
-window.onload = pageLoad;
-function createPuzzle() {
-  var puzArea = document.getElementById("puzzlearea");
-  var numSquares = puzArea.children.length;
-  for (var i = 0; i < numSquares; i++) {
-    var child = puzArea.children[i];
-    child.className += "puzzlepiece";
-    child.style.left = (SQSIZE * (i % COLS)) + "px";
-    child.style.top = SQSIZE * Math.floor((i / COLS)) + "px";
-  }
-}
+
+    function validMove(puzzlepiece){
+                BLOCKTOP=puzzlepiece.offsetTop;
+                BLOCKLEFT= puzzlepiece.offsetLeft;
+                var top= BLOCKTOP + "px";
+                var left= BLOCKLEFT + "px";
+
+                var testleft= Math.abs(parseInt(left) - parseInt(BLANKLEFT));
+                if (top == BLANKTOP && testleft==100){
+                        puzzlepiece.setAttribute("class", "puzzlepiece movablepiece");
+                        return true;
+
+                       }
+
+                var testright= Math.abs(parseInt(top) - parseInt(BLANKTOP));
+                if (left == BLANKLEFT && testright==100){
+                        puzzlepiece.setAttribute("class", "puzzlepiece movablepiece");
+                        return true;
+
+                       }
+
+    }
+};
